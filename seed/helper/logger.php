7fd3b2c8cca674f63012ef2a56706f901a2925dc
Rogerio Taques <rogerio.taques@rakuten.com>
2017-02-02 19:19:59 +0900

    Updates version

--- seed/helper/logger.php
+++ seed/helper/logger.php
@@ -3,7 +3,7 @@
  /* --------------------------------------------------------
  | Seed-PHP Microframework
  | @author Rogerio Taques (rogerio.taques@gmail.com)
- | @version 0.3.2
+ | @version 0.3.3
  | @license MIT
  | @see http://github.com/rogeriotaques/seed-php
  * -------------------------------------------------------- */
