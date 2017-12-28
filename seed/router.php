9ec2f789f30b28b9ba816fbad45ae4a45cbace65
Rogerio Taques <rogerio@awin.com.br>
2017-12-29 02:01:45 +0900

    Updates package version

--- seed/router.php
+++ seed/router.php
@@ -3,7 +3,7 @@
  /* --------------------------------------------------------
  | Seed-PHP Microframework.
  | @author Rogerio Taques (rogerio.taques@gmail.com)
- | @version 0.5.1
+ | @version 0.5.2
  | @license MIT
  | @see http://github.com/abtzco/seed-php
  * -------------------------------------------------------- */
