83252a4523c6e6e23c2568aeeb21de6e0e919efd
Rogerio Taques <rogerio@awin.com.br>
2018-07-19 18:38:27 +0900

    Update bundle version.

--- seed/core.php
+++ seed/core.php
@@ -3,7 +3,7 @@
  /* --------------------------------------------------------
  | Seed-PHP Microframework.
  | @author Rogerio Taques (rogerio.taques@gmail.com)
- | @version 0.7.6
+ | @version 0.7.7
  | @license MIT
  | @see http://github.com/abtzco/seed-php
  * -------------------------------------------------------- */
