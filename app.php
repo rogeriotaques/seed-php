30d14d9954c1cb9ca9711f2a55f9c4c8b16d3b77
Rogerio Taques <rogerio@jarvispro.local>
2018-10-30 17:41:31 +0900

    Updates version bundle

--- app.php
+++ app.php
@@ -3,7 +3,7 @@
  /* --------------------------------------------------------
  | Seed-PHP Microframework.
  | @author Rogerio Taques (hello@abtz.co)
- | @version 1.0.0
+ | @version 1.0.2
  | @license MIT
  | @see http://github.com/abtzco/seed-php
  * -------------------------------------------------------- */
