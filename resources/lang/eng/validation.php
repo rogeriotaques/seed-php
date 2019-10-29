a42407d1b9cd4e8284a042383d06dc3b3e493c2d
Douglas Lima <douglasproglima@gmail.com>
2019-10-30 08:19:37 +0900

    Included directory and file translation

---
+++ resources/lang/eng/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. 
+    | Some of these rules have multiple versions such as the size rules.
+    |
+    */
+
+return [
+    'accepted'             => 'The :attribute must be accepted.',
+    'confirmed'            => 'The :attribute confirmation does not match.',
+    'date'                 => 'The :attribute is not a valid date.',
+    'date_format'          => 'The :attribute does not match the format :format.',
+];
